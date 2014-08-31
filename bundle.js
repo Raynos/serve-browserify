8d916d46cc0a2b50b7f4d777893833787bb050ad
Raynos <raynos2@gmail.com>
2014-08-31 15:17:45 -0700

    add watchify option

--- bundle.js
+++ bundle.js
@@ -1,20 +1,47 @@
-var browserify = require("browserify")
-var embedNodeEnv = require("node-env/script")
+var browserify = require("browserify");
+var watchify = require("watchify");
+var extend = require("xtend");
+var embedNodeEnv = require("node-env/script");
 
-module.exports = bundle
+var locationCache = {};
+
+module.exports = bundle;
 
 // return javascript string for the payload!
 // bundle := (String, Callback<String>) => void
 function bundle(location, opts, callback) {
-    var b = browserify(opts)
-    b.add(location)
-    b.bundle(function (err, payload) {
+    var b
+    if (opts.watchify) {
+        b = getFromCache(location, opts);
+    } else {
+        b = getBundle(location, opts)
+    }
+
+    b.bundle(function onPayload(err, payload) {
         if (err) {
-            return callback(err)
+            return callback(err);
         }
 
-        payload = embedNodeEnv() + payload
+        payload = embedNodeEnv() + payload;
 
-        callback(null, payload)
-    })
+        callback(null, payload);
+    });
+}
+
+function getFromCache(location, opts) {
+    if (locationCache[location]) {
+        return locationCache[location];
+    }
+
+    var b = getBundle(location, extend(opts, watchify.args));
+    var w = watchify(b, opts);
+
+    locationCache[location] = w;
+    return w;
+}
+
+function getBundle(location, opts) {
+    var b = browserify(opts);
+    b.add(location);
+    return b
 }
